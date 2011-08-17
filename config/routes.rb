47d17563ee521597b04dff626f51c180813d4043
Benson Gu <benson.gu@sage.com>
2011-08-17 16:13:06 -0700

    Remove defaut index.html page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Sample::Application.routes.draw do
+  root              :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Sample::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  #root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
