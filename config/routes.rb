6b8be08f282b533b3c5609967708ce82a9e411ca
Benson Gu <benson.gu@sage.com>
2011-08-17 16:39:13 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Sample::Application.routes.draw do
-  root              :to => 'pages#home'
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  
+  root              :to => 'pages#home'
 
   
   # The priority is based upon order of creation:
